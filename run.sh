52d86b313a7ec916fda6ae04c3617b2057be1b58
Jörg Markert <joerg.markert@noris.de>
2022-07-16 13:11:39 +0200

    git + ipynb = :(

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir -p firmware/old/
+mv firmware/*.uf2 firmware/old/
+
 docker rm zmk
 docker run -it --name zmk zmk
 docker cp zmk:/app/firmware/ ./
 docker stop zmk
 docker rm zmk
+
+rsync -aHv --delete firmware ~/Nextcloud/zmk/firmware/
